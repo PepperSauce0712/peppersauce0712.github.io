2da5069b9d2e4a73588e69dbfde585e7a0e1ef01
PepperSauce0712 <150165966+PepperSauce0712@users.noreply.github.com>
2025-01-05 20:01:47 +0800

    v0.14
    + 新增關於我跟技能棧
    + 把網頁變成一頁式了
    + 新增MDX格式

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind({})],
+    integrations: [tailwind({}), mdx()],
     site: 'https://peppersauce0712.github.io/',
     base: '/PepperWebsite',
+    publicDir: './public',
 });
--- src/pages/portfolios/test.md
+++
@@ -1,9 +0,0 @@
----
-title: "UE 次元斬"
-author: "PepperSauce"
-destription: "次元斬"
-pubDate: 2024-12-25
-tags: ["Unreal Engine"]
----
-
-我的次元斬
