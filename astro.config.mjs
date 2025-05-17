174fe8798cae36fab97e9a67cea4c73fe70d3e7c
PepperSauce0712 <150165966+PepperSauce0712@users.noreply.github.com>
2025-05-17 21:50:45 +0800

    v0.3
    + 完成MDX中的TOC、標籤、時間
    之後要寫部落格的介面

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import remarkBreaks from 'remark-breaks';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
+import rehypePresetMinify from 'rehype-preset-minify';
 import mdx from '@astrojs/mdx';
 
-// https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind({}), mdx()],
+    integrations: [tailwind({}), mdx({
+      syntaxHighlight: 'shiki',
+      shikiConfig: { theme: 'dracula' },
+      remarkPlugins: [remarkBreaks],
+      rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'wrap' }], rehypePresetMinify],  
+      gfm: true,
+    })],
     site: 'https://PepperSauce0712.github.io/',
     base: '',
     publicDir: './public',
