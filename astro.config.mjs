9616de9c96a8c13cea60ada62c0c64211bccd27e
PepperSauce0712 <150165966+PepperSauce0712@users.noreply.github.com>
2025-05-23 21:44:34 +0800

    v0.37
    + 新增Astro-icon，很好用

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import rehypeSlug from 'rehype-slug';
 import rehypePresetMinify from 'rehype-preset-minify';
 import mdx from '@astrojs/mdx';
 
+import icon from 'astro-icon';
+
 export default defineConfig({
     integrations: [tailwind({}), mdx({
       syntaxHighlight: 'shiki',
@@ -14,7 +16,7 @@ export default defineConfig({
       remarkPlugins: [remarkBreaks],
       rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'wrap' }], rehypePresetMinify],  
       gfm: true,
-    })],
+    }), icon()],
     site: 'https://PepperSauce0712.github.io/',
     base: '',
     publicDir: './public',
