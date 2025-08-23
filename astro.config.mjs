99a0934c8c5dee03a1924dddd77a66b44ef64fad
PepperSauce <150165966+PepperSauce0712@users.noreply.github.com>
2025-08-23 23:13:43 +0800

    v0.41
    + 大改一波，把不好的架構改了

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import remarkBreaks from 'remark-breaks';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeSlug from 'rehype-slug';
@@ -8,16 +7,21 @@ import rehypePresetMinify from 'rehype-preset-minify';
 import mdx from '@astrojs/mdx';
 
 import icon from 'astro-icon';
-
+import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
-    integrations: [tailwind({}), mdx({
-      syntaxHighlight: 'shiki',
-      shikiConfig: { theme: 'dracula' },
-      remarkPlugins: [remarkBreaks],
-      rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'wrap' }], rehypePresetMinify],  
-      gfm: true,
-    }), icon()],
-    site: 'https://PepperSauce0712.github.io/',
-    base: '',
-    publicDir: './public',
+  integrations: [mdx({
+    syntaxHighlight: 'shiki',
+    shikiConfig: { theme: 'dracula' },
+    remarkPlugins: [remarkBreaks],
+    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'wrap' }], rehypePresetMinify],  
+    gfm: true,
+  }), icon()],
+
+  site: 'https://PepperSauce0712.github.io/',
+  base: '',
+  publicDir: './public',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
